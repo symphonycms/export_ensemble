afdcc384946c1e7fec95f991ccbe33f2b429d7b6
Nils Hörrmann <post@nilshoerrmann.de>
2010-01-09 20:03:30 +0100

    Improved localisation support

---
+++ lang/lang.pt-br.php
@@ -0,0 +1,46 @@
+<?php
+
+	$about = array(
+		'name' => 'Portuguese (Brazil)',
+		'author' => array(
+			'name' => 'Rainer Borene',
+			'email' => 'rainerborene@gmail.com',
+			'website' => false
+		),
+		'release-date' => '2009-10-08'
+	);
+	
+	
+	/*
+	 * EXTENSION: Export Ensemble
+	 * Localisation strings
+	 */
+
+	$dictionary = array(
+
+		'Existing Installation' => 
+		false,
+
+		'Install Symphony <em>Version %s</em>' => 
+		false,
+
+		'It appears that Symphony has already been installed at this location.' => 
+		false,
+
+		'Export Ensemble' =>
+		false,
+
+		'Create' =>
+		false,
+
+		'Packages entire site as a <code>.zip</code> archive for download.' =>
+		false,
+
+		'Export Ensemble cannot be installed, since the "<a href="http://php.net/manual/en/book.zip.php">ZipArchive</a>" class is not available. Ensure that PHP was compiled with the <code>--enable-zip</code> flag.' => 
+		false,
+
+		'Warning: It appears you do not have the "ZipArchive" class available. Ensure that PHP was compiled with <code>--enable-zip</code>' =>
+		false
+
+	);
+	
