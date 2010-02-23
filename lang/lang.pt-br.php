55ee9e151cba0be0c99683e7f0fe8d763a877829
Rainer Borene <rainerborene@gmail.com>
2010-02-23 08:01:43 +0800

    Added portuguese translation.

--- lang/lang.pt-br.php
+++ lang/lang.pt-br.php
@@ -4,8 +4,8 @@
 		'name' => 'Portuguese (Brazil)',
 		'author' => array(
 			'name' => 'Rainer Borene',
-			'email' => 'rainerborene@gmail.com',
-			'website' => false
+			'email' => 'me@rainerborene.com',
+			'website' => 'http://rainerborene.com/'
 		),
 		'release-date' => '2009-10-08'
 	);
@@ -19,28 +19,28 @@
 	$dictionary = array(
 
 		'Existing Installation' => 
-		false,
+		'Instalação existente',
 
 		'Install Symphony <em>Version %s</em>' => 
-		false,
+		'Instalar Symphony <em>Versão %s</em>',
 
 		'It appears that Symphony has already been installed at this location.' => 
-		false,
+		'Parece que o Symphony já foi instalado neste local.',
 
 		'Export Ensemble' =>
-		false,
+		'Exportar Ensemble',
 
 		'Create' =>
-		false,
+		'Criar',
 
 		'Packages entire site as a <code>.zip</code> archive for download.' =>
-		false,
+		'Compactar o site inteiro em um arquivo <code>.zip</code> para download.',
 
 		'Export Ensemble cannot be installed, since the "<a href="http://php.net/manual/en/book.zip.php">ZipArchive</a>" class is not available. Ensure that PHP was compiled with the <code>--enable-zip</code> flag.' => 
-		false,
+		'Export Ensemble não pode ser instalado, classe "<a href="http://php.net/manual/en/book.zip.php">ZipArchive</a>" não está disponível. Assegure-se que o PHP foi compilado com o parâmetro <code>--enable-zip</code>',
 
 		'Warning: It appears you do not have the "ZipArchive" class available. Ensure that PHP was compiled with <code>--enable-zip</code>' =>
-		false
+		'Atenção: Parece que você não tem a classe "ZipArchive" disponível. Assegure-se que o PHP foi compilado com o parâmetro <code>--enable-zip</code>'
 
 	);
 	
