d37deb2407dfd6fa5521e05a1bc25f0216c64a5c
Alistair Kearney <alistair@21degrees.com.au>
2010-05-21 15:51:19 +1000

    Updated to work with Symphony 2.1

--- extension.driver.php
+++ extension.driver.php
@@ -3,35 +3,79 @@
 	Class extension_export_ensemble implements iExtension{
 
 		public function about(){
-			return (object)array('name' => 'Export Ensemble',
-						 'version' => '1.11',
-						 'release-date' => '2010-02-12',
-						 'author' => (object)array('name' => 'Alistair Kearney',
-										   'website' => 'http://pointybeard.com',
-										   'email' => 'alistair@pointybeard.com')
-				 		);
+			return (object)array(
+				'name' => 'Export Ensemble',
+				'version' => '2.0',
+				'release-date' => '2010-05-21',
+				'author' => (object)array(
+					'name' => 'Alistair Kearney',
+					'website' => 'http://alistairkearney.com',
+					'email' => 'hi@alistairkearney.com'
+				)
+			);
 		}
 		
 		public function getSubscribedDelegates(){
 			return array(
-						array(
-							'page' => '/system/settings/',
-							'delegate' => 'AddCustomToolFieldsets',
-							'callback' => 'appendPreferences'
-						),
-
-					);
+				array(
+					'page' => '/system/settings/extensions/',
+					'delegate' => 'AddSettingsFieldsets',
+					'callback' => 'cbAppendPreferences'
+				),
+			
+				array(
+					'page' => '/system/settings/extensions/',
+					'delegate' => 'CustomSaveActions',
+					'callback' => 'cbSavePreferences'
+				),
+			);
 		}
 		
 		public function install(){
-			
 			if(!class_exists('ZipArchive')){
-				Administration::instance()->Page->pageAlert(__('Export Ensemble cannot be installed, since the "<a href="http://php.net/manual/en/book.zip.php">ZipArchive</a>" class is not available. Ensure that PHP was compiled with the <code>--enable-zip</code> flag.'), AdministrationPage::PAGE_ALERT_ERROR);
-				return false;
+				throw new ExtensionException(__(
+					'Export Ensemble cannot be installed, since the "ZipArchive" class is not available. Ensure that PHP was compiled with the --enable-zip flag.'
+				));
 			}
-			
 			return true;
 		}
+
+		public function cbSavePreferences($context){
+			$this->__export();
+		}
+
+		public function cbAppendPreferences($context){
+			
+			if(isset($_POST['action']['export'])){
+				$this->cbSavePreferences($context);
+			}
+			
+			$document = Administration::instance()->Page;
+			
+			$group = $document->createElement('fieldset');
+			$group->setAttribute('class', 'settings');
+			$group->appendChild($document->createElement('h3', __('Export Ensemble')));
+
+			$div = $document->createElement('div', NULL, array('id' => 'file-actions', 'class' => 'label'));
+			$span = $document->createElement('span');
+			
+			if(!class_exists('ZipArchive')){
+				$span->appendChild(
+					$document->createElement('p',  __('Warning: It appears you do not have the "ZipArchive" class available. Ensure that PHP was compiled with --enable-zip'))
+				);
+			}
+			else{
+				$span->appendChild($document->createElement('button', __('Create'), array('name' => 'action[export]', 'type' => 'submit')));	
+			}
+			
+			$div->appendChild($span);
+
+			$div->appendChild($document->createElement('p', __('Packages entire site as a <code>.zip</code> archive for download.'), array('class' => 'help')));	
+
+			$group->appendChild($div);
+			$context['fieldsets'][] = $group;
+						
+		}		
 		
 		private function __addFolderToArchive(&$archive, $path, $parent=NULL){
 			$iterator = new DirectoryIterator($path);
@@ -47,93 +91,38 @@
 		}
 		
 		private function __export(){
-			$sql_schema = $sql_data = NULL;
+			$sql = NULL;
 			
-			require_once(dirname(__FILE__) . '/lib/class.mysqldump.php');
+			require_once('lib/class.mysqldump.php');
 			
 			$dump = new MySQLDump(Symphony::Database());
-
-			$tables = array(
-				'tbl_authors',
-				'tbl_cache',
-				'tbl_entries',
-				'tbl_extensions',
-				'tbl_extensions_delegates',
-				'tbl_fields',
-				'tbl_fields_%',			
-				'tbl_forgotpass',
-				'tbl_pages',
-				'tbl_pages_types',
-				'tbl_sections',
-				'tbl_sections_association'			
-			);
 			
+			$tables = $dump->listTables(Symphony::Configuration()->db()->{'table-name-prefix'} . '%', false);
+			
+			$structure_tables = $tables;
+			$data_tables = $tables;
+			
+			//remove some tables from the $data list
+			$data_tables = array_flip($data_tables);
+			unset($data_tables['tbl_cache']);
+			unset($data_tables['tbl_forgotpass']);
+			unset($data_tables['tbl_users']);
+			unset($data_tables['tbl_sessions']);
+			$data_tables = array_flip($data_tables);
+
 			## Grab the schema
-			foreach($tables as $t) $sql_schema .= $dump->export($t, MySQLDump::STRUCTURE_ONLY);
-			$sql_schema = str_replace('`' . Administration::instance()->Configuration->get('tbl_prefix', 'database'), '`tbl_', $sql_schema);
-			
-			$sql_schema = preg_replace('/AUTO_INCREMENT=\d+/i', NULL, $sql_schema);
-			
-			$tables = array(
-				'tbl_entries',
-				'tbl_extensions',
-				'tbl_extensions_delegates',
-				'tbl_fields',			
-				'tbl_pages',
-				'tbl_pages_types',
-				'tbl_sections',
-				'tbl_sections_association'			
-			);			
-			
-			## Field data and entry data schemas needs to be apart of the workspace sql dump
-			$sql_data  = $dump->export('tbl_fields_%', MySQLDump::ALL);
-			$sql_data .= $dump->export('tbl_entries_%', MySQLDump::ALL);
-			
-			## Grab the data
-			foreach($tables as $t){
-				$sql_data .= $dump->export($t, MySQLDump::DATA_ONLY);
+			foreach($structure_tables as $t){
+				$sql .= $dump->export($t, MySQLDump::STRUCTURE_ONLY);
 			}
-			
-			$sql_data = str_replace('`' . Administration::instance()->Configuration->get('tbl_prefix', 'database'), '`tbl_', $sql_data);
-			
-			$config_string = NULL;
-			$config = Administration::instance()->Configuration->get();	
-			
-			unset($config['symphony']['build']);
-			unset($config['symphony']['cookie_prefix']);
-			unset($config['symphony']['file_write_mode']);
-			unset($config['symphony']['directory_write_mode']);
-			unset($config['database']['host']);
-			unset($config['database']['port']);
-			unset($config['database']['user']);
-			unset($config['database']['password']);
-			unset($config['database']['db']);
-			unset($config['database']['tbl_prefix']);
-			unset($config['region']['timezone']);
 
-			foreach($config as $group => $set){
-				foreach($set as $key => $val){
-					$config_string .= "		\$conf['{$group}']['{$key}'] = '{$val}';" . PHP_EOL;
-				}
+			## Grab the data
+			foreach($data_tables as $t){
+				$sql .= $dump->export($t, MySQLDump::DATA_ONLY);
 			}
 			
-			$install_template = str_replace(
-				
-									array(
-										'<!-- BUILD -->',
-										'<!-- VERSION -->',
-										'<!-- CONFIGURATION -->'
-									),
-				
-									array(
-										Administration::instance()->Configuration->get('build', 'symphony'),
-										Administration::instance()->Configuration->get('version', 'symphony'),
-										trim($config_string),										
-									),
-				
-									file_get_contents(dirname(__FILE__) . '/lib/installer.tpl')
-			);
-			
+			$sql = str_replace('`' . Symphony::Configuration()->db()->{'table-name-prefix'}, '`tbl_', $sql);
+			$sql = preg_replace('/AUTO_INCREMENT=\d+/i', NULL, $sql);
+
 			$archive = new ZipArchive;
 			$res = $archive->open(TMP . '/ensemble.tmp.zip', ZipArchive::CREATE);
 
@@ -142,10 +131,9 @@
 				$this->__addFolderToArchive($archive, EXTENSIONS, DOCROOT);
 				$this->__addFolderToArchive($archive, SYMPHONY, DOCROOT);
 				$this->__addFolderToArchive($archive, WORKSPACE, DOCROOT);
-				
-				$archive->addFromString('install.php', $install_template);
-				$archive->addFromString('install.sql', $sql_schema);
-				$archive->addFromString('workspace/install.sql', $sql_data);
+				$this->__addFolderToArchive($archive, DOCROOT . '/install', DOCROOT);
+
+				$archive->addFromString('install/assets/install.sql', $sql_schema);
 				
 				$archive->addFile(DOCROOT . '/index.php', 'index.php');
 				
@@ -171,7 +159,7 @@
 				sprintf(
 					'Content-disposition: attachment; filename=%s-ensemble.zip', 
 					Lang::createFilename(
-						Administration::instance()->Configuration->get('sitename', 'symphony')
+						Symphony::Configuration()->core->symphony->sitename
 					)
 				)
 			);
@@ -184,41 +172,6 @@
 			
 		}
 
-		public function __SavePreferences($context){
-			$this->__export();
-		}
-		
-		public function appendPreferences($context){
-			
-			if(isset($_POST['action']['export'])){
-				$this->__SavePreferences($context);
-			}
-			
-			$group = new XMLElement('fieldset');
-			$group->setAttribute('class', 'settings');
-			$group->appendChild(new XMLElement('legend', __('Export Ensemble')));			
-			
-
-			$div = new XMLElement('div', NULL, array('id' => 'file-actions', 'class' => 'label'));			
-			$span = new XMLElement('span');
-			
-			if(!class_exists('ZipArchive')){
-				$span->appendChild(
-					new XMLElement('p', '<strong>' . __('Warning: It appears you do not have the "ZipArchive" class available. Ensure that PHP was compiled with <code>--enable-zip</code>') . '<strong>')
-				);
-			}
-			else{
-				$span->appendChild(new XMLElement('button', __('Create'), array('name' => 'action[export]', 'type' => 'submit')));	
-			}
-			
-			$div->appendChild($span);
-
-			$div->appendChild(new XMLElement('p', __('Packages entire site as a <code>.zip</code> archive for download.'), array('class' => 'help')));	
-
-			$group->appendChild($div);						
-			$context['wrapper']->appendChild($group);
-						
-		}
 	}
 	
 	return 'extension_export_ensemble';
